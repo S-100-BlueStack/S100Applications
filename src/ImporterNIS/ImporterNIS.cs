791eb67d9ad8e19d11d65b32c147bdabc652c7d4
Jens Søe Christiansen <jenssc@pocalipse.dk>
2026-05-13 23:08:08 +0200

    bugfix: removed subtype creation!

--- src/ImporterNIS/ImporterNIS.cs
+++ src/ImporterNIS/ImporterNIS.cs
@@ -742,39 +742,39 @@ namespace S100Framework.Applications
                     schemaBuilder.Build();
                 }
                 {
-                    var features = Summary.PrimitiveFeatures(Primitives.noGeometry);
+                    //var features = Summary.PrimitiveFeatures(Primitives.noGeometry);
 
-                    var tableDefinition = destination.GetDefinition<TableDefinition>("featuretype");
+                    //var tableDefinition = destination.GetDefinition<TableDefinition>("featuretype");
 
-                    var tableDescription = new TableDescription(tableDefinition);
+                    //var tableDescription = new TableDescription(tableDefinition);
 
-                    var definitionReferences = new Dictionary<int, string> { { 0, "UNKNOWN" } };
-                    tableDescription.SubtypeFieldDescription = new SubtypeFieldDescription(tableDefinition.GetSubtypeField(), definitionReferences);
-                    schemaBuilder.Modify(tableDescription);
-                    schemaBuilder.Build();
+                    //var definitionReferences = new Dictionary<int, string> { { 0, "UNKNOWN" } };
+                    //tableDescription.SubtypeFieldDescription = new SubtypeFieldDescription(tableDefinition.GetSubtypeField(), definitionReferences);
+                    //schemaBuilder.Modify(tableDescription);
+                    //schemaBuilder.Build();
 
-                    foreach (var e in Summary.definitionReferenceFeatureTypes.Where(e => features.Contains(e.code)).OrderBy(e => e.code)) {
-                        definitionReferences.Add(e.sourceIdentifier, e.code);
-                    }
-                    tableDescription.SubtypeFieldDescription = new SubtypeFieldDescription(tableDefinition.GetSubtypeField(), definitionReferences);
-                    schemaBuilder.Modify(tableDescription);
-                    schemaBuilder.Build();
+                    //foreach (var e in Summary.definitionReferenceFeatureTypes.Where(e => features.Contains(e.code)).OrderBy(e => e.code)) {
+                    //    definitionReferences.Add(e.sourceIdentifier, e.code);
+                    //}
+                    //tableDescription.SubtypeFieldDescription = new SubtypeFieldDescription(tableDefinition.GetSubtypeField(), definitionReferences);
+                    //schemaBuilder.Modify(tableDescription);
+                    //schemaBuilder.Build();
                 }
                 {
-                    var tableDefinition = destination.GetDefinition<TableDefinition>("informationtype");
+                    //var tableDefinition = destination.GetDefinition<TableDefinition>("informationtype");
 
-                    var tableDescription = new TableDescription(tableDefinition);
+                    //var tableDescription = new TableDescription(tableDefinition);
 
-                    var definitionReferences = new Dictionary<int, string> { { 0, "UNKNOWN" } };
-                    schemaBuilder.Modify(tableDescription);
-                    schemaBuilder.Build();
+                    //var definitionReferences = new Dictionary<int, string> { { 0, "UNKNOWN" } };
+                    //schemaBuilder.Modify(tableDescription);
+                    //schemaBuilder.Build();
 
-                    foreach (var e in Summary.definitionReferenceInformationTypes.OrderBy(e => e.sourceIdentifier)) {
-                        definitionReferences.Add(e.sourceIdentifier, e.code);
-                    }
-                    tableDescription.SubtypeFieldDescription = new SubtypeFieldDescription(tableDefinition.GetSubtypeField(), definitionReferences);
-                    schemaBuilder.Modify(tableDescription);
-                    schemaBuilder.Build();
+                    //foreach (var e in Summary.definitionReferenceInformationTypes.OrderBy(e => e.sourceIdentifier)) {
+                    //    definitionReferences.Add(e.sourceIdentifier, e.code);
+                    //}
+                    //tableDescription.SubtypeFieldDescription = new SubtypeFieldDescription(tableDefinition.GetSubtypeField(), definitionReferences);
+                    //schemaBuilder.Modify(tableDescription);
+                    //schemaBuilder.Build();
                 }
                 //schemaBuilder.Build();
             }
